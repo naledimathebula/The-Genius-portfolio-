b5a6236ca0215249940d27595c8872134e14b2aa
naledimathebula <naledimathebula18@gmail.com>
2026-07-31 09:57:56 +0200

    new project added to cards,named error 404

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const progress = (scrollTop / scrollHeight) * 100;
+
+    document.getElementById("progress-bar").style.width =
+        progress + "%";
+};
